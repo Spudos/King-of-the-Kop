17fdd2104f84c1c3c44995351b49d24986cb2232
Andy Pierce <spudos16@gmail.com>
2023-12-04 10:38:40 +0000

    add js to check a comment has been submitted

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,26 @@ function validateForm() {
     alert("Please fill in your name");
     return false;
   }
-  
+
   const emailInput = document.getElementById("email");
-      const email = emailInput.value;
-      const emailFormat = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-      
-      if (!email.match(emailFormat)) {
-        alert("Please enter a valid email address");
-        return false;
-      }
+  const email = emailInput.value;
+  const emailFormat = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  
+  if (!email.match(emailFormat)) {
+    alert("Please enter a valid email address");
+    return false;
+  }
+
+  const commentsInput = document.getElementById("comments");
+  const comments = commentsInput.value;
+  
+  if (comments.trim() === "") {
+    alert("Please fill in your comments");
+    return false;
+  }
 
   const radioButtons = document.getElementsByName("feedbackType");
   let radioSelected = false;
-  console.log("test")
   for (let i = 0; i < radioButtons.length; i++) {
     if (radioButtons[i].checked) {
       radioSelected = true;
