993cf97be3f1b5aa85579d0e554e5ec4f8e9730e
Andy Pierce <spudos16@gmail.com>
2023-12-04 12:12:36 +0000

    change questions to an array in js and add 10 questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,20 +77,93 @@ return false;
 
 let score = 0;
 
-    const answerButtons = document.querySelectorAll('.answer-btn');
-    answerButtons.forEach(button => {
-      button.addEventListener('click', function() {
-        const selectedAnswer = this.textContent;
-        const correctAnswer = "Salah"; // Replace with the correct answer
+const questions = [
+  {
+    question: "Of the current players at the club, who has the most goals?",
+    options: ["Jota", "Salah", "Gomez"],
+    answer: "Salah"
+  },
+  {
+    question: "Who is the club captain?",
+    options: ["Trent", "Van Dijk", "Allison"],
+    answer: "Van Dijk"
+  },
+  {
+    question: "In the 2019 Champions League Semi Final, LFC were 3-0 down from the first leg before an amazing comeback saw them win 4-0 at home.  Who scored the 4th goal?",
+    options: ["Henderson", "Wijnaldum", "Origi"],
+    answer: "Origi"
+  },
+  {
+    question: "When the current Anfield road development is complete, what will be the approximate capacity of Anfield",
+    options: ["52000", "45000", "61000"],
+    answer: "61000"
+  },
+  {
+    question: "When Liverpool played against Man utd at Anfield in the 22/23 season, what was the score?",
+    options: ["3-0", "7-0", "10-0"],
+    answer: "7-0"
+  },
+  {
+    question: "Who did Liverpool beat 2-0 in the 2019 Champions League Final in Madrid?",
+    options: ["Tottenham", "Man City", "Napoli"],
+    answer: "Tottenham"
+  },
+  {
+    question: "How many times did Jamie Carragher play for Liverpool?",
+    options: ["508", "509", "510"],
+    answer: "508"
+  },
+  {
+    question: "How many times have Liverpool won Englands top division?",
+    options: ["13", "14", "19"],
+    answer: "19"
+  },
+  {
+    question: "What position did Liverpool finish in in 19/20?",
+    options: ["1st", "2nd", "3rd"],
+    answer: "1st"
+  },
+  {
+    question: "How many times have LFC won the FA Cup?",
+    options: ["4", "8", "9"],
+    answer: "8"
+  }
+];
 
-        if (selectedAnswer === correctAnswer) {
-          score++;
-          document.getElementById('scoreDisplay').textContent = score;
-          alert("Correct answer! Well done! Your score is now: " + score);
-          // Add further actions for a correct answer
-        } else {
-          alert("Incorrect answer. Please try again.");
-          // Add further actions for an incorrect answer
-        }
-      });
-    });
+const questionText = document.getElementById('questionText');
+const answerButtons = document.querySelectorAll('.answer-btn');
+
+let currentQuestionIndex = 0;
+
+function displayQuestion(index) {
+  questionText.textContent = questions[index].question;
+  for (let i = 0; i < answerButtons.length; i++) {
+    answerButtons[i].textContent = questions[index].options[i];
+  }
+}
+
+displayQuestion(currentQuestionIndex);
+
+answerButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    const selectedAnswer = this.textContent;
+    const correctAnswer = questions[currentQuestionIndex].answer;
+
+    if (selectedAnswer === correctAnswer) {
+      score++;
+      document.getElementById('scoreDisplay').textContent = score;
+      alert("Scored, marvellous strike!");
+    } else {
+      alert("A miss, Hit the bar!");
+    }
+
+    // Move to the next question
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+      displayQuestion(currentQuestionIndex);
+    } else {
+      alert("Quiz complete! Your final score is: " + score);
+      // Add further actions for quiz completion
+    }
+  });
+});
