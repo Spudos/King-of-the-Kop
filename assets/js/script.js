1d6a22d2d114597fe9723cbf15efd1b823bef03c
Andy Pierce <spudos16@gmail.com>
2023-12-04 10:30:13 +0000

    add js to check a radio button is selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.log("test")
+function validateForm() {
+  const radioButtons = document.getElementsByName("feedbackType");
+  let radioSelected = false;
+  console.log("test")
+  for (let i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+      radioSelected = true;
+      break;
+    }
+  }
+  if (!radioSelected) {
+    alert("Please select a feedback type");
+    return false;
+  }
+  return true;
+}
