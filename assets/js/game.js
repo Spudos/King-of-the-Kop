ed8fdcdd86fc2af0ac8b04b51af451daa9e19dd0
Andy Pierce <spudos16@gmail.com>
2023-12-06 15:10:40 +0000

    add penalty count and scoring

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,29 +1,49 @@
 let homePenalty = 0;
 let awayPenalty = 0;
+let iteration = 0;
 
-document.querySelectorAll('.block').forEach(block => {
-  block.addEventListener('click', () => {
-    const randomBlock = Math.floor(Math.random() * 4) + 1;
-    if (parseInt(block.id.slice(-1)) === randomBlock) {
+function handleBlockClick() {
+  const randomBlock = Math.floor(Math.random() * 4) + 1;
+  if (parseInt(this.id.slice(-1)) === randomBlock) {
+    document.getElementById('board').textContent = "You saved it!";
+    setTimeout(function() {
+      document.getElementById('board').textContent = "Match Score";
+    }, 3000);
+  } else {
+    awayPenalty++;
+    document.getElementById('awayPenaltyDisplay').textContent = awayPenalty;
+    document.getElementById('board').textContent = "They scored";
+    setTimeout(function() {
+      document.getElementById('board').textContent = "Match Score";
+    }, 3000);
+  }
+
+  setTimeout(() => {
+    const randomNum = Math.floor(Math.random() * 4) + 1;
+    if (randomNum > 2) {
       homePenalty++;
       document.getElementById('homePenaltyDisplay').textContent = homePenalty;
-
-    } else {
-      document.getElementById('board').textContent = "You missed!";
+      document.getElementById('board').textContent = "Your team scored!";
       setTimeout(function() {
         document.getElementById('board').textContent = "Match Score";
-      }, 2000);
+      }, 3000);
+    } else {
+      document.getElementById('board').textContent = "Your team missed!";
+      setTimeout(function() {
+        document.getElementById('board').textContent = "Match Score";
+      }, 3000);
     }
-    
-    setTimeout(() => {
-      const randomNum = Math.floor(Math.random() * 4) + 1;
-      if (randomNum > 2) {
-        awayPenalty++;
-        document.getElementById('awayPenaltyDisplay').textContent = awayPenalty;
 
-      } else {
-      
-      }
-    }, 3000);
-  });
+    iteration++;
+    if (iteration >= 5) {
+      document.querySelectorAll('.block').forEach(block => {
+        block.removeEventListener('click', handleBlockClick);
+      });
+    }
+  }, 3000);
+  document.getElementById('pens').textContent = iteration + 1;
+}
+document.querySelectorAll('.block').forEach(block => {
+  block.addEventListener('click', handleBlockClick);
 });
+
